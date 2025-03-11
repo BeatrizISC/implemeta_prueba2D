9839a9dfd214a13148511a28d8540f519df93472
BeatrizISC <beatriz.yz.pz@gmail.com>
2025-03-11 16:58:05 -0600

    Add files via upload

---
+++ programa1.py
@@ -0,0 +1,6 @@
+#Progama de la juguetería
+cant_mu=int(input("Escrbe la cantidad de muñecas a comprar"))
+cant_pa= float(input("Escribe la cantidad de payasos a comprar"))
+precio_total=cant_mu*75 + cant_pa*112
+print("El peso total del paquete es: ", "%.2f" precio_total)
+
